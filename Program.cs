103d74aec99fbace9d1cf07d98f8b7ddcac6cae4
NICKN1 <iranildo.junior@outlook.com>
2022-02-16 18:16:32 -0300

    Desafios 1 e 2 em C#

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Codigos_Sololearn
+{
+    class Program
+    {
+        static void codigo1()
+        {
+            /*============DESAFIO============
+            We are going to write a program that will calculate the area of ​​a circle.
+            The area enclosed by a circle of radius r is πr², where π (pi) is the constant ratio
+            of the circumference of any circle to its diameter, and r is the radius.
+            The given program declares a constant pi variable with a value of 3.14.
+            Complete the program to take the radius as input, then calculate and output the area 
+            of the circle.
+            */
+            //The value of const PI cannot be changed during program execution
+            const double pi = 3.14;
+            //Where radius take the input
+            double radius = double.Parse(Console.ReadLine());
+            double result = 0;
+            
+            result = radius * radius;
+            Console.WriteLine(result * pi);
+            
+        }
+
+        static void desafio2()
+        {
+            /*============DESAFIO============
+            You are an elementary school teacher and explaining multiplication to students.
+            You are going to use multiplication by 3 as your example.
+            The program you are given takes N number as input. Write a program to output all numbers
+            from 1 to N, replacing all numbers that are multiples of 3 by "*".
+            */
+            int number = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            
+            while(x <= number)
+            {
+                //minha condição, se o resto da divisão de X com 3 for igual a 0, continue
+                if(x % 3 == 0)
+                {
+                    Console.Write("*"); 
+                }
+                else{
+                Console.Write(x);
+                }
+                x++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
