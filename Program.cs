5c1015c6f279eb0e2f5459c14ad19d4a9ff8f913
NICKN1 <iranildo.junior@outlook.com>
2022-02-19 23:59:07 -0300

    Desafio de Contrutores e Propriedade de Classe

--- Program.cs
+++ Program.cs
@@ -114,6 +114,55 @@ namespace Codigos_Sololearn
 
         }
         }
+    
+    static void codigo4()
+    {
+        /*================DESAFIO==============
+        You are making a social network application and want to add post creation functionality.
+        As a user creates a post, the text "New post" should be automatically outputted so that then
+        the user can add the text he/she wants to share. The program you are given declares a Post 
+        class with a text private field, and the ShowPost() method which outputs the content.
+        Complete the class with
+        - a constructor, which outputs "New post" as called,
+        - Text property, which will allow you to get and set the value of the text field.
+        */
+
+        /*
+        static void Main(string[] args)
+        {
+            string postText = Console.ReadLine();
+
+            Post post = new Post();
+            post.Text = postText;
+            post.ShowPost();
+
+        }
+        */
+    /*class Post
+    {
+        private string text;
+        
+        //write a constructor here
+        public Post()
+        {
+            Console.WriteLine("New post");
+        }
+
+        public void ShowPost()
+        {
+            Console.WriteLine(text);
+        }
+        
+        //write a property for member text
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+        */
+    }
+    
     static void Main(string[] args)
         {
 
